f49b84c90110e15be09c5d7a1c37bcc734c67f90
gallakorea <gallakorea@gmail.com>
2025-12-10 20:29:07 +0900

    .

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,6 @@
-// supabase.js
-
 const SUPABASE_URL = "https://bidqaapputnhkeqvdzrr.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_XrZu5f09bGbs2SDAZ_fw_uJVAq-9E";
 
-// UMD에서는 이렇게 사용
-window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+const { createClient } = window.supabase;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
