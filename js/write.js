db2b4c87741c846a8888d9c2f5bf8ced4f768280
gallakorea <gallakorea@gmail.com>
2025-12-13 19:53:55 +0900

    .

--- js/write.js
+++ js/write.js
@@ -1,15 +1,39 @@
-const aiModal = document.getElementById('aiModal');
-document.getElementById('openAiModal').onclick = () => aiModal.style.display='flex';
-document.getElementById('aiClose').onclick = () => aiModal.style.display='none';
+const $ = id => document.getElementById(id);
 
-document.getElementById('thumbnailBtn').onclick = () => document.getElementById('thumbnail').click();
-document.getElementById('thumbnail').onchange = e => {
-  const img = document.querySelector('#thumbnailPreview img');
-  img.src = URL.createObjectURL(e.target.files[0]);
+thumbnailBtn.onclick = () => thumbnail.click();
+videoBtn.onclick = () => video.click();
+
+thumbnail.onchange = e => {
+  const f = e.target.files[0];
+  if (!f) return;
+  thumbnailPreview.style.display = 'block';
+  thumbnailPreview.querySelector('img').src = URL.createObjectURL(f);
 };
 
-document.getElementById('videoBtn').onclick = () => document.getElementById('video').click();
-document.getElementById('video').onchange = e => {
-  const v = document.querySelector('#videoPreview video');
-  v.src = URL.createObjectURL(e.target.files[0]);
+video.onchange = e => {
+  const f = e.target.files[0];
+  if (!f) return;
+  videoPreview.style.display = 'block';
+  const v = videoPreview.querySelector('video');
+  v.src = URL.createObjectURL(f);
+};
+
+openAiModal.onclick = () => {
+  aiModal.style.display = 'flex';
+  aiUserText.value = description.value;
+};
+aiClose.onclick = () => aiModal.style.display = 'none';
+
+runAi.onclick = () => {
+  aiResultText.value = aiUserText.value + "\n\n[AI 개선본]";
+};
+
+applyAi.onclick = () => {
+  description.value = aiResultText.value;
+  aiModal.style.display = 'none';
+};
+
+writeForm.onsubmit = e => {
+  e.preventDefault();
+  alert("미리보기 단계입니다");
 };
