f214333251f631bca03222af8538e0bab69e4ea5
gallakorea <gallakorea@gmail.com>
2025-12-13 19:23:10 +0900

    .

--- js/write.js
+++ js/write.js
@@ -1,21 +1,15 @@
 const aiModal = document.getElementById('aiModal');
-document.getElementById('openAiModal').onclick = () => aiModal.style.display = 'flex';
-document.getElementById('aiClose').onclick = () => aiModal.style.display = 'none';
+document.getElementById('openAiModal').onclick = () => aiModal.style.display='flex';
+document.getElementById('aiClose').onclick = () => aiModal.style.display='none';
 
 document.getElementById('thumbnailBtn').onclick = () => document.getElementById('thumbnail').click();
 document.getElementById('thumbnail').onchange = e => {
-  const img = document.querySelector('#thumbPreview img');
+  const img = document.querySelector('#thumbnailPreview img');
   img.src = URL.createObjectURL(e.target.files[0]);
-  document.getElementById('thumbPreview').style.display = 'block';
 };
 
 document.getElementById('videoBtn').onclick = () => document.getElementById('video').click();
 document.getElementById('video').onchange = e => {
-  const video = document.querySelector('#videoPreview video');
-  video.src = URL.createObjectURL(e.target.files[0]);
-  document.getElementById('videoPreview').style.display = 'block';
+  const v = document.querySelector('#videoPreview video');
+  v.src = URL.createObjectURL(e.target.files[0]);
 };
-
-document.querySelectorAll('.nav-item').forEach(btn => {
-  btn.onclick = () => location.href = btn.dataset.target;
-});
