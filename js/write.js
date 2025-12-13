3b2088104fbbf636ab754fe73393c28f1e1f97ef
gallakorea <gallakorea@gmail.com>
2025-12-13 19:57:30 +0900

    .

--- js/write.js
+++ js/write.js
@@ -1,39 +1,39 @@
 const $ = id => document.getElementById(id);
 
-thumbnailBtn.onclick = () => thumbnail.click();
-videoBtn.onclick = () => video.click();
+$("thumbnailBtn").onclick = () => $("thumbnail").click();
+$("videoBtn").onclick = () => $("video").click();
 
-thumbnail.onchange = e => {
-  const f = e.target.files[0];
-  if (!f) return;
-  thumbnailPreview.style.display = 'block';
-  thumbnailPreview.querySelector('img').src = URL.createObjectURL(f);
+$("thumbnail").onchange = e => {
+  const file = e.target.files[0];
+  if (!file) return;
+  $("thumbPreview").src = URL.createObjectURL(file);
+  $("thumbPreview").style.display = "block";
 };
 
-video.onchange = e => {
-  const f = e.target.files[0];
-  if (!f) return;
-  videoPreview.style.display = 'block';
-  const v = videoPreview.querySelector('video');
-  v.src = URL.createObjectURL(f);
+$("video").onchange = e => {
+  const file = e.target.files[0];
+  if (!file) return;
+  $("videoPreview").src = URL.createObjectURL(file);
+  $("videoPreview").style.display = "block";
 };
 
-openAiModal.onclick = () => {
-  aiModal.style.display = 'flex';
-  aiUserText.value = description.value;
-};
-aiClose.onclick = () => aiModal.style.display = 'none';
-
-runAi.onclick = () => {
-  aiResultText.value = aiUserText.value + "\n\n[AI 개선본]";
+$("description").oninput = e => {
+  document.querySelector(".desc-counter").innerText =
+    `${e.target.value.length} / 500`;
 };
 
-applyAi.onclick = () => {
-  description.value = aiResultText.value;
-  aiModal.style.display = 'none';
+$("openAiModal").onclick = () => {
+  $("aiUserText").value = $("description").value;
+  $("aiModal").style.display = "flex";
 };
 
-writeForm.onsubmit = e => {
-  e.preventDefault();
-  alert("미리보기 단계입니다");
+$("aiClose").onclick = () => $("aiModal").style.display = "none";
+
+$("runAi").onclick = () => {
+  $("aiResultText").value = "[AI 결과 예시]\n" + $("aiUserText").value;
+};
+
+$("applyAi").onclick = () => {
+  $("description").value = $("aiResultText").value;
+  $("aiModal").style.display = "none";
 };
