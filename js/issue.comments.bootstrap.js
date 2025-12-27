c66c5797cb17f69c9534cd61f9aa85f5b6b5e616
gallakorea <gallakorea@gmail.com>
2025-12-27 18:22:43 +0900

    Update issue.comments.bootstrap.js

--- js/issue.comments.bootstrap.js
+++ js/issue.comments.bootstrap.js
@@ -1,10 +1,10 @@
 import { getComments, setComments } from "./issue.comments.state.js";
 import { renderCommentsUI, renderBattleDashboard } from "./issue.comments.ui.js";
 import { calculateBattleStats, applyCombatLogic } from "./issue.comments.engine.js";
-import { fetchComments } from "./issue.comments.api.js";
+import { loadCommentsAPI } from "./issue.comments.api.js";
 
 export async function startCommentSystem(issueId) {
-  const raw = await fetchComments(issueId);
+  const raw = await loadCommentsAPI(issueId);
 
   const units = applyCombatLogic(raw);
   setComments(units);
