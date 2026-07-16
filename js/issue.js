bae347c1441d5b261bd29dfc7427937528b512a8
gallakorea <gallakorea@gmail.com>
2026-07-16 13:49:52 +0900

    chore(seo): 구글 서치콘솔 소유확인 메타 추가

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=071970";
-import { loadAiNews } from "./issue-news.js?v=071970";
-import { loadStats } from "./issue.stats.js?v=071970";
-import { initCommentSystem } from "./issue.comments.js?v=071970";
+import { loadAiArguments } from "./issue-argument.js?v=071971";
+import { loadAiNews } from "./issue-news.js?v=071971";
+import { loadStats } from "./issue.stats.js?v=071971";
+import { initCommentSystem } from "./issue.comments.js?v=071971";
 
 
 console.log("[issue.js] loaded");
