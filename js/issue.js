bf904c5c1e7f53e2beb21168572f8a4d1c3cfd23
gallakorea <gallakorea@gmail.com>
2026-07-24 10:46:40 +0900

    style(dm): 프로스트 입력바 유리 톤 상향 — 순흑 위에서도 또렷하게

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072378";
-import { loadAiNews } from "./issue-news.js?v=072378";
-import { loadStats } from "./issue.stats.js?v=072378";
-import { initCommentSystem } from "./issue.comments.js?v=072378";
+import { loadAiArguments } from "./issue-argument.js?v=072379";
+import { loadAiNews } from "./issue-news.js?v=072379";
+import { loadStats } from "./issue.stats.js?v=072379";
+import { initCommentSystem } from "./issue.comments.js?v=072379";
 
 
 console.log("[issue.js] loaded");
