fea24fd749ec8f8c0f8f6d28f0ae2694dd0408f4
gallakorea <gallakorea@gmail.com>
2026-07-27 20:15:29 +0900

    diag(ui): track 밀림 진단 v072729

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072728";
-import { loadAiNews } from "./issue-news.js?v=072728";
-import { loadStats } from "./issue.stats.js?v=072728";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072728";
+import { loadAiArguments } from "./issue-argument.js?v=072729";
+import { loadAiNews } from "./issue-news.js?v=072729";
+import { loadStats } from "./issue.stats.js?v=072729";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072729";
 
 
 console.log("[issue.js] loaded");
