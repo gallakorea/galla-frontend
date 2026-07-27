7a86bb75731107b9e3fee405a22fc709031de203
gallakorea <gallakorea@gmail.com>
2026-07-27 20:18:10 +0900

    diag+fix(ui): interactive-widget=resizes-content 뷰포트 v072730

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072729";
-import { loadAiNews } from "./issue-news.js?v=072729";
-import { loadStats } from "./issue.stats.js?v=072729";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072729";
+import { loadAiArguments } from "./issue-argument.js?v=072730";
+import { loadAiNews } from "./issue-news.js?v=072730";
+import { loadStats } from "./issue.stats.js?v=072730";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072730";
 
 
 console.log("[issue.js] loaded");
