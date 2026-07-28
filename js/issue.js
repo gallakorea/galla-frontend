181f196688a0f82e7e088066bbbe268b60aef543
gallakorea <gallakorea@gmail.com>
2026-07-28 11:12:18 +0900

    fix(call): getMedia 멈춤 복구 5초→2초 단축(전환 지연 감소) v072767

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072766";
-import { loadAiNews } from "./issue-news.js?v=072766";
-import { loadStats } from "./issue.stats.js?v=072766";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072766";
+import { loadAiArguments } from "./issue-argument.js?v=072767";
+import { loadAiNews } from "./issue-news.js?v=072767";
+import { loadStats } from "./issue.stats.js?v=072767";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072767";
 
 
 console.log("[issue.js] loaded");
