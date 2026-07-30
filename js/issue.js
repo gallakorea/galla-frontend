ef03756093048bb9dd5e9832cde8e74e6393909c
gallakorea <gallakorea@gmail.com>
2026-07-30 23:25:49 +0900

    chore: v072912 (OTA 실테스트용 버전 범프) · sw v461

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072911";
-import { loadAiNews } from "./issue-news.js?v=072911";
-import { loadStats } from "./issue.stats.js?v=072911";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072911";
+import { loadAiArguments } from "./issue-argument.js?v=072912";
+import { loadAiNews } from "./issue-news.js?v=072912";
+import { loadStats } from "./issue.stats.js?v=072912";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072912";
 
 
 console.log("[issue.js] loaded");
