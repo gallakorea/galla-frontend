cd22300d5aaa9b63744a9d79a45963bc585eb6ff
gallakorea <gallakorea@gmail.com>
2026-07-31 00:23:02 +0900

    diag(boot): 문서로드 진단 비콘(navType/referrer) · v072917 · sw v466

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072916";
-import { loadAiNews } from "./issue-news.js?v=072916";
-import { loadStats } from "./issue.stats.js?v=072916";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072916";
+import { loadAiArguments } from "./issue-argument.js?v=072917";
+import { loadAiNews } from "./issue-news.js?v=072917";
+import { loadStats } from "./issue.stats.js?v=072917";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072917";
 
 
 console.log("[issue.js] loaded");
