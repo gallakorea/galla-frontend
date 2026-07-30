cfadb08ed56c38f03c9aa8805c8b93d1353b1012
gallakorea <gallakorea@gmail.com>
2026-07-31 01:29:30 +0900

    fix(splash): 진입 index.html=SPA에서 스플래시 정상 표시(옛 skip 제거) · v072923

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=072922";
-import { loadAiNews } from "./issue-news.js?v=072922";
-import { loadStats } from "./issue.stats.js?v=072922";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072922";
+import { loadAiArguments } from "./issue-argument.js?v=072923";
+import { loadAiNews } from "./issue-news.js?v=072923";
+import { loadStats } from "./issue.stats.js?v=072923";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=072923";
 
 
 console.log("[issue.js] loaded");
