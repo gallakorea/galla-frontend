46f039ee630256926484e2b11ee988051f2bdbf3
gallakorea <gallakorea@gmail.com>
2026-07-31 23:44:19 +0900

    fix: 버전 073122 앞자리 0 복원(정수변환으로 73122 됐던 것)

--- js/issue.js
+++ js/issue.js
@@ -1,7 +1,7 @@
-import { loadAiArguments } from "./issue-argument.js?v=73122";
-import { loadAiNews } from "./issue-news.js?v=73122";
-import { loadStats } from "./issue.stats.js?v=73122";
-import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=73122";
+import { loadAiArguments } from "./issue-argument.js?v=073122";
+import { loadAiNews } from "./issue-news.js?v=073122";
+import { loadStats } from "./issue.stats.js?v=073122";
+import { initCommentSystem, destroyCommentSystem } from "./issue.comments.js?v=073122";
 
 
 console.log("[issue.js] loaded");
